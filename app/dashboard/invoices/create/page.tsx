169351df49bcfaa4609a89c6800c9c8314691c0c
Dario Milicic <dario.milicic@toptal.com>
2024-03-25 20:15:03 +0100

    Added auth and final touches

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -18,7 +18,7 @@ export default async function Page() {
           },
         ]}
       />
-      <Form customers={customers} action={createInvoice} />
+      <Form customers={customers} />
     </main>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'NextJS Dashboard',
+  description:
+    'The NextJS dashboard built for learning purposes by Dario Milicic.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
